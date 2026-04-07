fbbfae0f40cfa90f4f040de1cf8b221902463fa3
biliiciucmaxim <maxim.biliiciuc@rwth-aachen.de>
2026-04-07 20:19:45 +0200

    initialisation

---
+++ src/main/java/org/learning_management_system/showcase/ShowCaseApplication.java
@@ -0,0 +1,13 @@
+package org.learning_management_system.showcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowCaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShowCaseApplication.class, args);
+    }
+
+}

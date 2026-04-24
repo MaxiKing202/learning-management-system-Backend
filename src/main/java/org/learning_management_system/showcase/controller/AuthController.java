bc49310ce48f1a722cd70b5c29da2f8421cb1c40
biliiciucmaxim <maxim.biliiciuc@rwth-aachen.de>
2026-04-24 19:57:39 +0200

    deleted debug line

--- src/main/java/org/learning_management_system/showcase/controller/AuthController.java
+++ src/main/java/org/learning_management_system/showcase/controller/AuthController.java
@@ -27,7 +27,6 @@ public class AuthController {
         if (user.isPresent()) {
             return ResponseEntity.ok("Token generated!");
         } else {
-            System.out.println("name " + credentials.getUsername() + " " + credentials.getPassword());
             return ResponseEntity.status(401).body("Invalid credentials");
         }
     }
